b03f2953692182af8bca485645908cd313fe237c
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-03 19:18:35 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//эта функция находит схожие с вашими введённми буквами с буквами предложенных слов.
 document.querySelector('#elastic').oninput = function () {
     let val = this.value.trim();
     let elasticItems = document.querySelectorAll('.elastic li');
@@ -5,19 +6,23 @@ document.querySelector('#elastic').oninput = function () {
         elasticItems.forEach(function (elem) {
             if (elem.innerText.search(val) == -1) {
                 elem.classList.add('hide');
+                elem.innerHTML = elem.innerText;
             }
             else {
                 elem.classList.remove('hide');
                 var str = elem.innerText;
+                elem.innerHTML = inserMark(str, elem.innerText.search(val), val.length);
             }
         });
     } else {
         elasticItems.forEach(function (elem) {
             elem.classList.remove('hide');
+            elem.innerHTML = elem.innerText;
         });
     }
 }
 
+//данная функция маркерует схожие буквы введённые вами с буквами слов.
 function inserMark (string, pos, len) {
     //lol
     //l<mark>o</mark>l
