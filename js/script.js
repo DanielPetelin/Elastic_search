bd0223df7e8f350254affba57b2aefa018748eaa
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-03 19:04:03 +0300

    or

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-document.querySelector('#elastic').oninput = function() {
-    var val = this.value.trim();
-    var elasticItems = document.querySelectorAll('.elastic li');
+document.querySelector('#elastic').oninput = function () {
+    let val = this.value.trim();
+    let elasticItems = document.querySelectorAll('.elastic li');
     if (val != '') {
-        elasticItems.forEach(function(elem) {
+        elasticItems.forEach(function (elem) {
             if (elem.innerText.search(val) == -1) {
                 elem.classList.add('hide');
-            };
+            }
+            else {
+                elem.classList.remove('hide');
+            }
         });
-    };
-};
+    } else {
+        elasticItems.forEach(function (elem) {
+            elem.classList.remove('hide');
+        });
+    }
+}
