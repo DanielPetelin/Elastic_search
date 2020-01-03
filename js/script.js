0815a3aa48135d043cf307dcb2464f24ccec8d40
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-03 18:37:06 +0300

    none

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 document.querySelector('#elastic').oninput = function() {
     var val = this.value.trim();
     var elasticItems = document.querySelectorAll('.elastic li');
-     
+    if (val != '') {
+        elasticItems.forEach(function(elem) {
+            if (elem.innerText.search(val) == -1) {
+                elem.classList.add('hide');
+            };
+        });
+    };
 };
